c873ac2aaaca762ace18a7da1f056e24eab458c1
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-04-26 23:34:32 -0700

    Finish all locator.

--- src/main/java/com/jemonjam/versioneer/impl/FileVersionLocator.java
+++ src/main/java/com/jemonjam/versioneer/impl/FileVersionLocator.java
@@ -4,13 +4,26 @@
 package com.jemonjam.versioneer.impl;
 
 import com.jemonjam.versioneer.api.VersionLocator;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class FileVersionLocator implements VersionLocator {
+    private final String path;
+
+    public FileVersionLocator(final String path) {
+        this.path = path;
+    }
 
     @Override
     public final Optional<String> getVersion() {
-        return Optional.empty();
+        try {
+            String version = new String(Files.readAllBytes(Paths.get(path)));
+            return Optional.ofNullable(version);
+        } catch (IOException e) {
+            return Optional.empty();
+        }
     }
 
 }
--- src/main/java/com/jemonjam/versioneer/impl/GitVersionLocator.java
+++ src/main/java/com/jemonjam/versioneer/impl/GitVersionLocator.java
@@ -4,13 +4,25 @@
 package com.jemonjam.versioneer.impl;
 
 import com.jemonjam.versioneer.api.VersionLocator;
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 public class GitVersionLocator implements VersionLocator {
 
     @Override
     public final Optional<String> getVersion() {
-        return Optional.empty();
+        try {
+            String path = ClassLoader.getSystemClassLoader().getResource(".").getPath();
+            Git git = Git.open(new File(path));
+            String version = git.describe().call();
+            return Optional.ofNullable(version);
+        } catch (IOException | GitAPIException e) {
+            return Optional.empty();
+        }
+
     }
 
 }
