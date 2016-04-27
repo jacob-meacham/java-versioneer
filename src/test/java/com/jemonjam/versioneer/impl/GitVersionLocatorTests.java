1eed6b4c596aa795cc95d178e0f253b6485c029d
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-04-27 11:48:50 -0700

    Make git locator test more resilient.

--- src/test/java/com/jemonjam/versioneer/impl/GitVersionLocatorTests.java
+++ src/test/java/com/jemonjam/versioneer/impl/GitVersionLocatorTests.java
@@ -4,19 +4,45 @@
 
 package com.jemonjam.versioneer.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GitVersionLocatorTests {
+    private Path tempDirectory;
+
+    @Before
+    public final void setup() throws IOException {
+        tempDirectory = Files.createTempDirectory("versioneer").toAbsolutePath();
+    }
+
+    @After
+    public final void teardown() throws IOException {
+        FileUtils.deleteDirectory(tempDirectory.toFile());
+    }
+
     @Test
-    public final void testGetsVersion() {
-        GitVersionLocator locator = new GitVersionLocator();
+    public final void testGetsVersion() throws IllegalStateException, GitAPIException {
+        final String versionString = "10.1.100-abc";
+
+        final Git git = Git.init().setDirectory(tempDirectory.toFile()).call();
+        git.commit().setAllowEmpty(true).setMessage("root commit").call();
+        git.tag().setAnnotated(true).setName(versionString).setMessage(versionString).call();
+
+        GitVersionLocator locator = new GitVersionLocator(tempDirectory.toString());
         Optional<String> version = locator.getVersion();
 
-        assertTrue(version.isPresent());
+        assertEquals(versionString, version.get());
     }
 
     @Test
